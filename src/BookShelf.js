ae632658ed948a48a9651154755312f243733ae4
DRT <daniriian@gmail.com>
2018-06-07 15:26:52 +0300

    added books to shelf

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -21,7 +21,12 @@ class BookShelf extends Component {
                         {this.props.books.map((book) => (
                             <li className="book-list-item" key={book.id}>
                                 <div className="book">
-                                    {book.title}
+                                    <div className="book-top">
+                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail}` }}>
+                                        </div>
+                                    </div>
+                                    <div className="book-title">{book.title}</div>
+                                    <div className="book-authors">{book.authors ? book.authors.join(', ') : ''}</div>
                                 </div>
                             </li>
                         ))}
