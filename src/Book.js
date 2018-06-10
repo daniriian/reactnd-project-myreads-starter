07cf1b6a748efb26902587a50468cd0ed34b073d
DRT <daniriian@gmail.com>
2018-06-10 13:19:15 +0300

    add onChange event to Book component

--- src/Book.js
+++ src/Book.js
@@ -8,7 +8,7 @@ class Book extends Component {
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail}` }}>
                     </div>
                     <div className="book-shelf-changer">
-                        <select>
+                        <select value={book.shelf} onChange={(e)=>this.updateBook(e.target.value)}>
                             <option value="move" disabled>Move to...</option>
                             <option value="currentlyReading">Currently Reading</option>
                             <option value="wantToRead">Want to Read</option>
