449b8555e0c47f66eeac6b918b5f63dbd039985f
DRT <daniriian@gmail.com>
2018-06-11 10:08:37 +0300

    make Book component a functional component

--- src/Book.js
+++ src/Book.js
@@ -1,42 +1,36 @@
-import React, {Component} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-//Book component
-class Book extends Component {
-    static propTypes = {
-        book: PropTypes.object.isRequired,
-        onUpdateBook: PropTypes.func.isRequired
-    }
-
-    updateBook (selectOption) {
-        this.props.onUpdateBook(this.props.book, selectOption)
-    }
-
-    render() {
-        return (
-            <div className="book">
-                <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, 
-                        //book cover
-                        backgroundImage: `url(${this.props.book.imageLinks!==undefined ? this.props.book.imageLinks.thumbnail: ''}` }}>
-                    </div>
-                    <div className="book-shelf-changer">
-                    {/* book shelf changer */}
-                        <select value={this.props.book.shelf === undefined ? "none" : this.props.book.shelf} onChange={(e)=>this.updateBook(e.target.value)}>
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                        </select>
-                    </div>
+//Book component - this is a functional component
+const Book = ({ book, onUpdateBook }) => {
+    var updateBook = (selectOption) => { onUpdateBook(book, selectOption) }
+    const styles = { width: 128, height: 193, backgroundImage: `url(${book.imageLinks !== undefined ? book.imageLinks.thumbnail : ''}` }
+    return (
+        < div className="book" >
+            <div className="book-top">
+                <div className="book-cover" style={styles}>
                 </div>
-                {/* book title */}
-                <div className="book-title">{this.props.book.title}</div>
-                {/* book authors */}
-                <div className="book-authors">{this.props.book.authors ? this.props.book.authors.join(', ') : ''}</div>
+                <div className="book-shelf-changer">
+                    {/* book shelf changer */}
+                    <select value={book.shelf === undefined ? "none" : book.shelf} onChange={(e) => updateBook(e.target.value)}>
+                        <option value="move" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                    </select>
+                </div>
             </div>
-        )
-    }
+            {/* book title */}
+            <div className="book-title" > {book.title}</div>
+            {/* book authors */}
+            < div className="book-authors" > {book.authors ? book.authors.join(', ') : ''}</div >
+        </div >
+    );
 }
 
-export default Book
+Book.propTypes = {
+    book: PropTypes.object.isRequired,
+    onUpdateBook: PropTypes.func.isRequired
+}
+
+export default Book;
---
+++ src/BookClass.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+//Book component
+
+class Book extends Component {
+    static propTypes = {
+        book: PropTypes.object.isRequired,
+        onUpdateBook: PropTypes.func.isRequired
+    }
+
+    updateBook(selectOption) {
+        this.props.onUpdateBook(this.props.book, selectOption)
+    }
+
+    render() {
+        const style = { width: 128, height: 139, backgroundImage: `url(${this.props.book.imageLinks !== undefined ? this.props.book.imageLinks.thumbnail : ''}` }
+        return (
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={style}>
+                    </div>
+                    <div className="book-shelf-changer">
+                        {/* book shelf changer */}
+                        <select value={this.props.book.shelf === undefined ? "none" : this.props.book.shelf} onChange={(e) => this.updateBook(e.target.value)}>
+                            <option value="move" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
+                    </div>
+                </div>
+                {/* book title */}
+                <div className="book-title">{this.props.book.title}</div>
+                {/* book authors */}
+                <div className="book-authors">{this.props.book.authors ? this.props.book.authors.join(', ') : ''}</div>
+            </div>
+        )
+    }
+}
+
+export default Book
